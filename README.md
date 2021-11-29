d00d38dbbcfece65fa0709e0de060278fd8105a8
Engo092 <Enzokusuki@gmail.com>
2021-11-29 17:59:55 -0300

    Fully functional Rock-Paper-Scissors game with UI, everything is now working and the functionalities are 100% finished

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rock Paper Scissors (UI version)
 
-This project showcases some Javascript capabilites, including DOM manipulation. It is a relatively simple program that simulates a Rock Paper Scissors game 5 times, and prints the overall winner at the end (feature still being developed in UI). Please keep in mind that the CSS of this page is pretty simple, as it is meant to represent Javascript capabilities more than anything else.
+This project showcases some Javascript capabilites, mostly DOM manipulation. It is a relatively simple program that simulates a Rock Paper Scissors game 5 times, and prints the overall winner at the end. Please keep in mind that the CSS of this page is pretty simple, as it is meant to represent Javascript capabilities more than anything else.
 
-In this game the user plays against a computer, there are 3 buttons in the page, each labeled as "Rock", "Paper" or "Scissors". After the player makes their own choice a message will display telling them if they won that round. After 5 rounds a definitive winner will be shown, but that feature is currently being developed and for now the player will just be playing forever.
+In this game the user plays against a computer, there are 3 buttons in the page, each labeled as "Rock", "Paper" and "Scissors". After the player makes their own choice a message will display telling them if they won that round. After 5 rounds a definitive winner will be shown and the player won't be able to continue playing that round, though they can restart the game if they want by clicking the restart button.
--- script.js
+++ script.js
@@ -1,18 +1,51 @@
+let playerScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
+
+// Starts variables for displaying scores
+const playerDiv = document.querySelector(".player");
+const computerDiv = document.querySelector(".computer");
+playerDiv.textContent = `Player Score: ${playerScore}`;
+computerDiv.textContent = `Computer Score: ${computerScore}`;
+
+// Makes restart button initially hidden
+let restart = document.querySelector(".restart");
+restart.setAttribute('style', "visibility: hidden;")
+
+const buttons = document.querySelectorAll('#button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        computer = computerPlay();
+        let winner = playRound(button.className, computer);
+
+        computeRound(winner);
+    });
+});
+
+const restartEvent = document.querySelector(".restartButton");
+restartEvent.addEventListener('click', () => {
+    
+    restartGame();
+});
+
+
 function computerPlay() {
     let choice = Math.random();
 
     if (choice <= 0.33) {
         return "rock";
-    }
+    };
 
     if (choice <= 0.66) {
         return "paper";
-    }
+    };
 
     if (choice <= 0.99) {
         return "scissors";
-    }
-}
+    };
+};
+
 
 function playRound(player, computer) {
 
@@ -21,85 +54,97 @@ function playRound(player, computer) {
     if (player === computer) {
         div.textContent = "It is a tie!";
         return "tie";
-    }
+    };
 
     if (player === "rock" && computer === "scissors") {
         div.textContent = "You Win! Rock beats Scissors";
         return "player";
-    }
+    };
 
     if (player === "scissors" && computer === "rock") {
         div.textContent = "You Lose! Rock beats Scissors";
         return "computer";
-    }
+    };
 
     if (player === "rock" && computer === "paper") {
         div.textContent = "You Lose! Paper beats Rock";
         return "computer";
-    }
+    };
 
     if (player === "paper" && computer === "rock") {
         div.textContent = "You Win! Paper beats Rock";
         return "player";
-    }
+    };
 
     if (player === "paper" && computer === "scissors") {
         div.textContent = "You Lose! Scissors beats Paper";
         return "computer";
-    }
+    };
 
     if (player === "scissors" && computer === "paper") {
         div.textContent = "You Win! Scissors beats Paper";
         return "player";
-    }
-} 
+    };
+};
 
 
-function game() {
+function computeRound(winner) {
+    gamesPlayed++;
+    if (winner == "player") {
+        playerScore++;
+        playerDiv.textContent = `Player Score: ${playerScore}`;
+    };
 
-    let computerScore = 0;
-    let playerScore = 0;
+    if (winner == "computer") {
+        computerScore++;
+        computerDiv.textContent = `Computer Score: ${computerScore}`;
+    };
 
-    // for (let i = 0; i < 5; i++) {
-    //     let computerChoice = computerPlay();
-    //     let playerChoice;
-    
-    //     while (true) {
-    //         playerChoice = prompt("Please choose what to play:").toLowerCase();
-    //         playerChoice = playerChoice.trim();
-    
-    //         if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
-    //             break;
-    //         }
-    //     }
+    if (gamesPlayed == 5) {
+        buttons.forEach((button) => {
 
-    //     let win = playRound(playerChoice, computerChoice);
+            // Following modifications are made so that user has visual clues indicating buttons aren't working
+            button.disabled = true;
+            button.removeAttribute("id");
+            button.setAttribute('style', "cursor: not-allowed;")
+        });
+        const div = document.querySelector('.result');
+        div.classList.add("winner");
 
-    //     if (win === "player") {
-    //         playerScore++;
-    //     }
-    //     if (win === "computer") {
-    //         computerScore++;
-    //     }
-    // }
+        if (playerScore > computerScore) {
+            div.textContent = "Player Wins!";
+        }
+        else if (computerScore > playerScore) {
+            div.textContent = "Computer Wins!";
+        }
+        else {
+            div.textContent = "Tie!";
+        };
 
-    if (computerScore > playerScore) {
-        console.log("Computer Wins!");
-    }
-    else if (playerScore > computerScore) {
-        console.log("Player Wins!");
-    }
-    else {
-        console.log("Tie!");
-    }
-}
+        // Turns restart button visible after 5 rounds
+        restart.setAttribute('style', "visibility: visible;")
+    };
+};
 
-//game();
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        computer = computerPlay();
-        playRound(button.className, computer);
+// This function basically restarts page to the same state as when it was loaded
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    gamesPlayed = 0;
+
+    restart.setAttribute('style', "visibility: hidden;")
+
+    let div = document.querySelector(".result");
+    div.classList.remove("winner");
+    div.textContent = "⠀";
+
+    buttons.forEach((button) => {
+        button.disabled = false;
+        button.setAttribute("id", "button");
+        button.style.removeProperty("cursor");
     });
-});
+    
+    playerDiv.textContent = `Player Score ${playerScore}`;
+    computerDiv.textContent = `Computer Score ${computerScore}`;
+};
